c8cb9b9e578d5a5e38a5a88b10a5127cabe70be4
psricky <psrickyking@gmail.com>
2026-04-20 01:11:21 +0530

    expense tracker backend api of the login

--- expenseApp/controller/userController.js
+++ expenseApp/controller/userController.js
@@ -1,26 +1,26 @@
 const NewUsers = require("../models/user");
 
-const userEntry= async(req,res)=>{
+const userEntry = async (req, res) => {
     try {
-        const {username,email,password}=req.body;
-        if(!username || !email || !password){
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
             return res.status(400).json({
                 success: false,
                 message: "Fields are mandatory"
             })
         }
-        const user=await NewUsers.findOne({
-            where:{
-                email:email
+        const user = await NewUsers.findOne({
+            where: {
+                email: email
             }
         })
-        if(user){
+        if (user) {
             return res.status(409).json({
                 success: false,
                 message: "user already exists"
             })
         }
-        const newEntry=await NewUsers.create({username,email,password});
+        const newEntry = await NewUsers.create({ username, email, password });
         return res.status(201).json({
             success: true,
             message: "User registered successfully",
@@ -28,7 +28,7 @@ const userEntry= async(req,res)=>{
         })
 
     } catch (error) {
-        console.log(error)        
+        console.log(error)
         return res.status(500).json({
             success: false,
             message: "internal server error"
@@ -36,39 +36,44 @@ const userEntry= async(req,res)=>{
     }
 }
 
-const userLogin=async (req,res)=>{
+const userLogin = async (req, res) => {
     try {
-        const {email,password}=req.body;
-        if( !email || !password ){
+        const { email, password } = req.body;
+        if (!email || !password) {
             return res.status(400).json({
                 success: false,
                 message: "Fields are mandatory"
             })
         }
-        const user=await NewUsers.findOne({
-            where:{
-                email:email,
-                password:password
+        const user = await NewUsers.findOne({
+            where: {
+                email: email
             }
         })
-        if(!user){
-            return res.status(404).send('Invalid user or password')
-        }else{
-           return res.status(200).json({
-            message:'User logged in successfully',
-            success:true
-           }) 
+        if (!user) return res.status(404).json({
+            message: 'User not found'
+        })
+        if (user.password === password) {
+            return res.status(200).
+                json({
+                    message: 'User login successful',
+                    success: true
+                })
+        } else {
+            return res.status(401).json({
+                message: 'User not authorized'
+            })
         }
 
     } catch (error) {
-        console.log(error)        
+        console.log(error)
         return res.status(500).json({
             success: false,
             message: "internal server error"
         });
     }
 }
-module.exports={
+module.exports = {
     userEntry,
     userLogin
 }
--- expenseApp/public/index.js
+++ expenseApp/public/index.js
@@ -5,9 +5,9 @@ signupform.addEventListener('submit', async (e) => {
     const username = document.getElementById('username').value
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-    
+
     try {
-       const res= await axios.post(`${url}/user/signup`, {
+        const res = await axios.post(`${url}/user/signup`, {
             username,
             email,
             password
@@ -17,23 +17,27 @@ signupform.addEventListener('submit', async (e) => {
         console.log(err)
     }
 })
-const signinform=document.getElementById('signinform')
+const signinform = document.getElementById('signinform')
 signinform.addEventListener('submit', async (e) => {
     e.preventDefault()
-   
+
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-    
+
     try {
-       const res= await axios.post(`${url}/user/signin`, {
+        const res = await axios.post(`${url}/user/signin`, {
             email,
             password
         })
         console.log(res)
+        if (!password) {
+            alert('User not authorized')
+        }
         alert('User logged in successfully')
     } catch (err) {
         console.log(err)
-        alert('Invalid user or password')
+        alert('User not found')
+
     }
 })
 
